4a3c88230e8f3da54fb2c4fbdf43f83e95c34101
silentesc <dominic.woehrle@gmail.com>
2023-12-25 02:41:05 +0100

    Implement logic, tasks & changed utils for LocalDateTime instead of LocalTime

---
+++ src/main/java/de/silentesc/restarter/tasks/RestartScheduler.java
@@ -0,0 +1,17 @@
+package de.silentesc.restarter.tasks;
+
+import de.silentesc.restarter.Main;
+import org.bukkit.Bukkit;
+
+import java.time.temporal.ChronoUnit;
+
+public class RestartScheduler extends ScheduleHelper {
+    public void scheduleRestart() {
+        // Time to next restart in seconds
+        long secondsUntilRestart = ChronoUnit.SECONDS.between(getNowDateTime(), getRestartDateTime());
+
+        // Schedule restart
+        Bukkit.getScheduler().runTaskLater(Main.getINSTANCE(), () ->
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "restart"), ((long) 20 * secondsUntilRestart));
+    }
+}
